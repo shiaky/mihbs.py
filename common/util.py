511f87642c60df59e6f9e7be63d78bcce5096aaa
shiaky <some-body@somewhere.com>
2018-01-30 12:57:02 +0100

    added stats export and plot function to sensitivity analyser

--- common/util.py
+++ common/util.py
@@ -2,6 +2,7 @@
 
 import cv2
 import os
+import string
 
 
 def escape_home_in_path(sPath):
@@ -52,3 +53,22 @@ def list_all_images_in_directory(sDirectoryPath):
     # filter image files
     return [sDirectoryPathEscaped +
             m for m in aFiles if m.lower().endswith((".png", ".bmp", ".jpg", ".jpeg", ".tiff"))]
+
+
+def format_filename(s):
+    """
+        Thanks to Sean Hammond (https://github.com/seanh)
+        Take a string and return a valid filename constructed from the string.
+        Uses a whitelist approach: any characters not present in valid_chars are
+        removed. Also spaces are replaced with underscores.
+
+        Note: this method may produce invalid filenames such as ``, `.` or `..`
+        When I use this method I prepend a date string like '2009_01_15_19_46_32_'
+        and append a file extension like '.txt', so I avoid the potential of using
+        an invalid filename.
+
+        """
+    valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+    filename = ''.join(c for c in s if c in valid_chars)
+    filename = filename.replace(' ', '_')  # I don't like spaces in filenames.
+    return filename
--- sensitivity/analyser.py
+++ sensitivity/analyser.py
@@ -8,6 +8,8 @@ import dbcon
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import seaborn as sb
+import matplotlib.pyplot as plt
 
 
 # ---- helper functions ------
@@ -26,7 +28,17 @@ def convert_db_to_pandas(sPathToDB="../data/sensitivity_results/sensitivity_resu
     return oSensitivityTestData
 
 
-def calculate_stats(oSensitivityTestData, lTestDatasetSize, lRandomDataSplitSeed=None, aFARThresholdSteps=np.arange(0, 1.01, 0.01)):
+def save_pandas_to_file(oPandasDataframe, sPath, sFilebaseName):
+    """ save a pandas dataframe to txt, csv and tex """
+    with open(sPath + sFilebaseName + ".txt", 'w') as file:
+        oPandasDataframe.to_string(file)
+    with open(sPath + sFilebaseName + ".tex", 'w') as file:
+        oPandasDataframe.to_latex(file)
+    with open(sPath + sFilebaseName + ".csv", 'w') as file:
+        oPandasDataframe.to_csv(file, index=False)
+
+
+def calculate_stats(oSensitivityTestData, lTestDatasetSize, lRandomDataSplitSeed=None):
 
     dicResult = {}
 
@@ -70,18 +82,82 @@ def calculate_stats(oSensitivityTestData, lTestDatasetSize, lRandomDataSplitSeed
                 np.array(aHashDeviations) < dThreshold)
             aErrorRate.append(lCountOfValuesSmallerThreshold /
                               lNumberOfTestReferenceRelationsTotal)
-        oErrorRate = pd.DataFrame(
-            {"threshold": aThresholdSteps, "errorrate": aErrorRate})
 
-        dicResult[sHashAlgo] = {"metrics": dicMetrics, "errorrate": oErrorRate}
+        dicResult[sHashAlgo] = {"metrics": dicMetrics, "errorrate": aErrorRate}
     return dicResult
 
 
-#-------------------------------
+def plot_FAR(dicSensitivityResults):
+    """plot FAR """
+
+    sPlotPath = sSensitivityTestResultsBasePath + "plots/"
+    util.create_path(sPlotPath)
+
+    for sHashType, dicMetrics in dicSensitivityResults.items():
+        aFAR = dicMetrics["errorrate"]
+        pdERRData = pd.DataFrame(
+            {"Threshold": aThresholdSteps, "Errorrate": aFAR, "Type": ["FAR"] * len(aThresholdSteps)})
+        pdERRData['subject'] = 0
+
+        # calc less FAR FRR value
+        lFARNotNullValueX = aThresholdSteps[np.argmax(np.array(aFAR) > 0) - 1]
+
+        plt.clf()
+        sTitle = sHashType
+        sb.set_style("whitegrid")
+        oSeabornPlot = sb.tsplot(time="Threshold", value="Errorrate",
+                                 condition="Type", unit="subject", interpolate=True, data=pdERRData)
+        oSeabornPlot.set(title=sTitle)
+        oSeabornPlot.set(xlabel="Threshold")
+        oSeabornPlot.set(ylabel="Errorrate")
+        oSeabornPlot.set(xticks=np.arange(0, 1.01, 0.1))
+        oSeabornPlot.set(yticks=np.arange(0, 1.01, 0.1))
+        oSeabornPlot.set(ylim=(0, 1))
+        # add not zero lines
+        plt.axvline(x=lFARNotNullValueX, color='r', linestyle="--")
+        sFilenameSafeTitle = util.format_filename(sTitle)
+        oSeabornPlot.get_figure().savefig(sPlotPath + sFilenameSafeTitle + ".png")
+        plt.clf()
+
+
+def save_stats(dicSensitivityResults):
+    """ write metrics to file """
+    sStatsPath = sSensitivityTestResultsBasePath + "stats/"
+    util.create_path(sStatsPath)
+
+    oPandasMetrics = pd.DataFrame(
+        columns=['hashalgorithm', 'min', 'max', 'mean', 'percentile_25', 'percentile_75'])
+    oPandasErrorrates = pd.DataFrame(
+        columns=['hashalgorithm', 'threshold', 'errorrate'])
+
+    for sHashType, dicMetrics in dicSensitivityResults.items():
+        # add metrics line to overview pandas dataframe
+        oPandasMetrics = oPandasMetrics.append({
+            'hashalgorithm': sHashType,
+            'min': dicMetrics["metrics"]["min"],
+            'max': dicMetrics["metrics"]["max"],
+            'mean': dicMetrics["metrics"]["mean"],
+            'percentile_25': dicMetrics["metrics"]["p25"],
+            'percentile_75': dicMetrics["metrics"]["p75"]}, ignore_index=True)
+
+        # add errorrates to file
+        oPandasErrorrates = pd.concat([oPandasErrorrates, pd.DataFrame({
+            'hashalgorithm': [sHashType] * len(aThresholdSteps),
+            'threshold': aThresholdSteps,
+            'errorrate': dicMetrics["errorrate"]})])
+
+    # write metrics to files
+    sMetricsFileName = "hash_algorithm_metrics"
+    save_pandas_to_file(oPandasMetrics, sStatsPath, sMetricsFileName)
+
+    # write errorrates to file
+    sErrorratesFileName = "hash_algorithm_errorrates_raw"
+    save_pandas_to_file(oPandasErrorrates, sStatsPath, sErrorratesFileName)
+
+    #-------------------------------
 
 
 if __name__ == "__main__":
-
     # ---------- config ----------------------
 
     # set name of collection to test
@@ -104,6 +180,14 @@ if __name__ == "__main__":
     # define the steps for the calculation of the error rate
     # and the plot (min, max + step, step)
     aThresholdSteps = np.arange(0, 1.01, 0.01)
+
+    # define path for plots and starts
+    sSensitivityTestResultsBasePath = "../data/sensitivity_results/"
+
+    # set size of the plot
+    # set figure size
+    plt.figure(num=None, figsize=(7, 6), dpi=100,
+               facecolor='w', edgecolor='k')
 
     # ----------------------------------------
 
@@ -134,13 +218,13 @@ if __name__ == "__main__":
     # from every image from the test dataset to
     # every image in the simila image dataset
     # for every hashing algo
-    # calc number of hits for thresholds (0, 0.5)
+    # calc number of hits for thresholds (0, ?)
     # (threaded)
     dicSensitivityResults = calculate_stats(oSensitivityTestData,
-                                            lTestDatasetSize, lRandomDataSplitSeed, aThresholdSteps)
+                                            lTestDatasetSize, lRandomDataSplitSeed)
 
-    # plot min mean max for every hashing algo
-    # TODO: plot
+    # plot FAR for every hashing algo
+    plot_FAR(dicSensitivityResults)
 
-    # export min, mean, max for every hashing algo
-    # TODO: export
+    # export min, mean, max  and errorrate for every hashing algo
+    save_stats(dicSensitivityResults)
